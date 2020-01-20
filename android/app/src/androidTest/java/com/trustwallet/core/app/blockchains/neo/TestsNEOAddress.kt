febc6a995d99c99c52c62b8ef4dc83aaab29df66
svyatoslav.d <svyatoslav.d@blaize.tech>
2020-01-20 16:24:22 +0200

    use AnyAddress in android NEO test

--- android/app/src/androidTest/java/com/trustwallet/core/app/blockchains/neo/TestsNEOAddress.kt
+++ android/app/src/androidTest/java/com/trustwallet/core/app/blockchains/neo/TestsNEOAddress.kt
@@ -9,10 +9,7 @@ package com.trustwallet.core.app.blockchains.neo
 import com.trustwallet.core.app.utils.Numeric
 import org.junit.Assert.assertEquals
 import org.junit.Test
-import wallet.core.jni.NEOAddress
-import wallet.core.jni.PrivateKey
-import wallet.core.jni.PublicKey
-import wallet.core.jni.PublicKeyType
+import wallet.core.jni.*
 
 class TestNEOAddress {
 
@@ -23,7 +20,7 @@ class TestNEOAddress {
     @Test
     fun testAddressFromPublicKey() {
         val pubkey = PublicKey(Numeric.hexStringToByteArray("0222b2277d039d67f4197a638dd5a1d99c290b17aa8c4a16ccee5165fe612de66a"), PublicKeyType.NIST256P1)
-        val address = NEOAddress(pubkey)
+        val address = AnyAddress(pubkey)
         val expectedAddressString = "AKmrAHRD9ZDUnu4m3vWWonpsojo4vgSuqp"
         assertEquals(address.description(), expectedAddressString)
     }
@@ -31,7 +28,7 @@ class TestNEOAddress {
     @Test
     fun testAddressFromString() {
         val addressString = "AXkgwcMJTy9wTAXHsbyhauxh7t2Tt31MmC"
-        val address = NEOAddress(addressString)
+        val address = AnyAddress(addressString)
         assertEquals(address.description(), addressString)
     }
 
@@ -39,7 +36,7 @@ class TestNEOAddress {
     fun testAddressFromPrivateKey() {
         val key = PrivateKey(Numeric.hexStringToByteArray("2A9EAB0FEC93CD94FA0A209AC5604602C1F0105FB02EAB398E17B4517C2FFBAB"))
         val pubkey = key.publicKeyNist256p1
-        val address = NEOAddress(pubkey)
+        val address = AnyAddress(pubkey)
         val expectedAddressString = "AQCSMB3oSDA1dHPn6GXN6KB4NHmdo1fX41"
 
         assertEquals(address.description(), expectedAddressString)
